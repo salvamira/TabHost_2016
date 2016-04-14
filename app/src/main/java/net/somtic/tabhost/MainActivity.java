7dcc8264b68abd6a3c0ac14c76c2b59ed7f1c45e
Salva Mira <smira@iesperemaria.com>
2016-04-14 09:51:33 +0200

    Ejercicio 2.11 finalizado

--- app/src/main/java/net/somtic/tabhost/MainActivity.java
+++ app/src/main/java/net/somtic/tabhost/MainActivity.java
@@ -1,13 +1,38 @@
 package net.somtic.tabhost;
 
+import android.app.TabActivity;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = getResources();
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+
+        /* pest1 es una tag que nos sirve para identificar la pestaña.
+           No se muestra. Idem pest2 y pest3 */
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("pest1");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("pest2");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("pest3");
+
+        tab1.setIndicator(res.getString(R.string.eti_tab1), null);
+        tab2.setIndicator(res.getString(R.string.eti_tab2), null);
+        tab3.setIndicator(res.getString(R.string.eti_tab3), null);
+
+        tab1.setContent(R.id.tab1Layout);
+        tab2.setContent(R.id.tab2Layout);
+        tab3.setContent(R.id.tab3Layout);
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
     }
+
 }
